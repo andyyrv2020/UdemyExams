e58096b687aed420094812164f6d84b379f356e0
PC-23 <it.pgee.burgas@gmail.com>
2024-01-16 16:23:30 +0200

    .

---
+++ StringInterpolation/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace StringInterpolation
+{
+	public class Program
+	{
+		public static string FormatDate(int year, int month, int day)
+		{
+			return $"{year}/{month}/{day}";
+		}
+	}
+}
